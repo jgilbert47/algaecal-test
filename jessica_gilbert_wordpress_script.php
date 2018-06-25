d30572dbf1ec00a8dcb63322d057cdcce3ea24d6
Jessica Gilbert <jessica.gilbert47@yahoo.com>
2018-06-25 17:58:30 -0500

    Version Correction
    Corrected the jQuery version number in the replace_jquery() function

--- jessica_gilbert_wordpress_script.php
+++ jessica_gilbert_wordpress_script.php
@@ -7,7 +7,7 @@ function replace_jquery() {
         // Deregister the default version of jQuery that Wordpress includes locally
         wp_deregister_script('jquery');
         // Register the latest version of jQuery from the Google Hosted CDN
-        wp_register_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js', false, '1.8.1');
+        wp_register_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js', false, '3.3.1');
     }
 }
 add_action('init', 'replace_jquery');
